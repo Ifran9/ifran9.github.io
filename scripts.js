34e08cec50054043095f96eb99201c8c605f38b6
ifodenet <ifodenetfrancois1@gmail.com>
2024-06-25 14:08:45 +0200

    first commit

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
+// Ajouter un événement de soumission au formulaire de contact
 document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Votre message a été envoyé avec succès!');
+    event.preventDefault(); // Empêcher la soumission du formulaire
+    alert('Votre message a été envoyé avec succès!'); // Afficher un message de succès
     // Vous pouvez ajouter ici le code pour envoyer le formulaire via AJAX
 });
